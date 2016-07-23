16e56d84718fd237f6eaf76c7478aa2ff9c4fdaf
nikthequik <quik.the.nick@gmail.com>
2016-07-23 01:35:12 -0600

    -institute build system and hold most files in app -include build folder

---
+++ countryDetail/countryDetail.js
@@ -0,0 +1,43 @@
+angular.module('cc-app')
+.controller('DetailCtrl', ['$routeParams', 'getData', 'allCountriesModel', '$location', function($routeParams, getData, allCountriesModel, $location){
+	var dc = this;
+	
+	dc.country = JSON.parse($routeParams.country);
+	dc.allCountries = [];
+	
+	dc.goToCountry = function(neighbor) {
+		angular.forEach(allCountriesModel.countries, function(item){
+			if (item.countryCode === neighbor.countryCode) {
+				console.log(item);
+				$location.path('/countries/' + JSON.stringify(item));
+			}
+		});
+	}
+	dc.getCapital = function(featureCode, country) {
+		var params = {
+			"country": country,
+			"featureCode": featureCode,
+		}
+		getData('searchJSON', params, false)
+		.then(function(data){
+			dc.toggleCapPop = data.geonames[0]===undefined ? 0 : 1;
+			dc.capital = data.geonames[0]===undefined ? "No Capital" : data.geonames[0].name;
+			dc.capPop = data.geonames[0]===undefined ? "No Inhabitants" : data.geonames[0].population;
+			console.log(dc.capPop);
+		})
+	};
+	dc.getNeighbors = function(params) {
+		getData('neighboursJSON', {geonameId: params}, false)
+		.then(function(data){
+			if (data.geonames) {
+				dc.neighborCount = data.geonames.length;
+				dc.neighbors = data.geonames;
+			}
+			else {
+				dc.neighborCount = "No";
+			}
+		})
+	};
+	dc.getNeighbors(dc.country.geonameId);
+	dc.getCapital("PPLC", dc.country.countryCode);
+}]);
---
+++ js/cc-app-lib.js
@@ -0,0 +1,33 @@
+angular.module('cc-app')
+
+.constant('CC_APP_USERNAME', 'nikthequik')
+.constant('CC_APP_PREFIX', 'http://api.geonames.org/')
+
+.factory('getData', ['$http', '$q', 'CC_APP_PREFIX', 'CC_APP_USERNAME', function($http, $q, CC_APP_PREFIX, CC_APP_USERNAME){
+	return function(endpoint, params, cache){
+		var data = null;
+		params = (typeof params==='object') ? params : {};
+		params = params===null ? {} : params;
+		params.username=CC_APP_USERNAME;
+		if (cache){
+			data = localStorage.getItem(endpoint);
+		}
+		if (!data) {
+			var url = CC_APP_PREFIX + endpoint;
+			return $http.get(url, {params: params})
+			.then (function(res){
+				if (cache){
+					localStorage.setItem(endpoint, JSON.stringify(res.data));
+				}
+				return res.data;
+			})
+		}
+		return $q.when(JSON.parse(data));
+	}
+}])
+
+.factory('allCountriesModel', function(){
+	return {
+		'countries': []
+	}
+});
